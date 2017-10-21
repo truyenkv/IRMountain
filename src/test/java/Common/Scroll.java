bc64e76f71b7b4b4dbda73d50d319ba15b041b35
Truyen <kieuvantruyen.3dfish@gmail.com>
2017-10-21 22:53:27 +0700

    fg

---
+++ src/test/java/Common/Scroll.java
@@ -0,0 +1,12 @@
+package Common;
+
+import net.serenitybdd.screenplay.actions.Evaluate;
+
+import org.openqa.selenium.WebElement;
+
+public class Scroll {
+	public void scrollToElement(WebElement element)
+	{
+		
+	}
+}
